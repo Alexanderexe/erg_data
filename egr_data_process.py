e3c41d08e839389bba36b2d1b41584fc58cddd1f
crax <cronergy100@mail.ru>
2021-07-07 01:37:16 +0300

    add base functionality

---
+++ egr_data_process.py
@@ -0,0 +1,70 @@
+import os
+
+import pandas as pd
+from posixpath import join
+
+
+def get_value(data, value):
+    if type(data) == dict:
+        return data[value]
+
+
+class EgrDataProcess:
+    ROOT_DIR = "DATA"
+    ADRESSES_DIR = join(ROOT_DIR, "getAddressByPeriod")
+    EVENTS_DIR = join(ROOT_DIR, "getEventByPeriod")
+    SHORT_INFO = join(ROOT_DIR, "getShortInfoByPeriod")
+    BASE_INFO = join(ROOT_DIR, "getBaseInfoByPeriod")
+    VED_DIR = join(ROOT_DIR, "getVEDByPeriod")
+    MODEL_DIR = join(ROOT_DIR, "erg_model")
+
+    @staticmethod
+    def concat_files(directory):
+        df = pd.DataFrame()
+        for file in os.listdir(directory):
+            df = df.append(pd.read_csv(os.path.join(directory, file)))
+        return df
+
+    def process_adresses(self):
+        adresses = self.concat_files(self.ADRESSES_DIR)
+        adresses = adresses[
+            ['ngrn', 'dfrom', 'dto', 'nindex', 'nsi00202', 'vregion', 'vdistrict', 'vnp', 'vtels', 'vemail', 'vsite',
+             'vfax']]
+        adresses.loc[:, 'vnsfull'] = adresses.loc[:, 'nsi00202'].apply(lambda x: get_value(x, 'vnsfull'))
+        adresses = adresses.drop(columns=[i for i in adresses.columns if 'nsi' in i])
+        adresses.to_csv(join(self.MODEL_DIR, 'adresses.csv'), index=False)
+
+    def process_events(self):
+        events = self.concat_files(self.EVENTS_DIR)
+        events = events[
+            ['ngrn', 'ddoc', 'vdocn', 'dsrok', 'dfrom', 'dto', 'nsi00223', 'nsi00212R', 'nsi00213', 'nsi00212',
+             'vprim']]
+        events.loc[:, 'decision_code'] = events.loc[:, 'nsi00223'].apply(lambda x: get_value(x, 'vnop'))
+        events.loc[:, 'decision_name'] = events.loc[:, 'nsi00223'].apply(lambda x: get_value(x, 'nsi00223'))
+        events.loc[:, 'decision_nkuz'] = events.loc[:, 'nsi00212R'].apply(lambda x: get_value(x, 'nkuz'))
+        events.loc[:, 'basis_code'] = events.loc[:, 'nsi00213'].apply(lambda x: get_value(x, 'nkosn'))
+        events.loc[:, 'basis_name'] = events.loc[:, 'nsi00213'].apply(lambda x: get_value(x, 'vnosn'))
+        events.loc[:, 'doc_nkuz'] = events.loc[:, 'nsi00212'].apply(lambda x: get_value(x, 'nkuz'))
+        events = events.drop(columns=[i for i in events.columns if 'nsi' in i])
+        events.to_csv(join(self.MODEL_DIR, 'events.csv'), index=False)
+
+    def process_base_info(self):
+        base_info = self.concat_files(self.BASE_INFO)
+        base_info = base_info[['ngrn', 'dfrom', 'dto', 'nsi00219', 'nsi00211', 'nsi00212']]
+        base_info.loc[:, 'status'] = base_info.loc[:, 'nsi00219'].apply(lambda x: get_value(x, 'vnsostk'))
+        base_info.loc[:, 'type'] = base_info.loc[:, 'nsi00211'].apply(lambda x: get_value(x, 'nkvob'))
+        base_info.loc[:, 'vnuzp'] = base_info.loc[:, 'nsi00212'].apply(lambda x: get_value(x, 'vnuzp'))
+        base_info = base_info.drop(columns=[i for i in base_info.columns if 'nsi' in i])
+        base_info.to_csv(join(self.MODEL_DIR, 'base_info.csv'), index=False)
+
+    def process_short_info(self):
+        short_info = self.concat_files(self.SHORT_INFO)
+        short_info = short_info[['vfio', 'ngrn', 'vnaim']]
+        short_info.to_csv(join(self.MODEL_DIR, 'short_info.csv'), index=False)
+
+    def process_ved(self):
+        ved = self.concat_files(self.VED_DIR)
+        ved.loc[:, 'ved_name'] = ved.loc[:, 'nsi00114'].apply(lambda x: get_value(x, 'vnvdnp'))
+        ved.loc[:, 'ved_code'] = ved.loc[:, 'nsi00114'].apply(lambda x: get_value(x, 'vkvdn'))
+        ved = ved.drop(columns=[i for i in ved.columns if 'nsi' in i])
+        ved.to_csv(join(self.MODEL_DIR, 'ved.csv'), index=False)
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from io import BytesIO
 from enum import Enum
-from datetime import datetime
+from datetime import datetime, date
+import os
 
+from posixpath import join
 import pycurl
 import pandas as pd
 
+from egr_data_process import EgrDataProcess
+
 
 class ErgMethod(Enum):
+    SHORT_INFO = "getShortInfoByPeriod"
     ADDRESS = "getAddressByPeriod"
     EVENTS = "getEventByPeriod"
     BASE_INFO = "getBaseInfoByPeriod"
-    SHORT_INFO = "getShortInfoByPeriod"
     VED = "getVEDByPeriod"
+
+
+class ErgRunMode(Enum):
+    PARTIAL_UPDATE = 1
+    FULL_UPDATE = 2
+    PROCESS = 3
 
 
 def download_egr_data(method, start_date, end_date):
@@ -26,15 +36,68 @@ def download_egr_data(method, start_date, end_date):
     return out
 
 
+def format_date(date_for_format):
+    format_pattern = '%d.%m.%Y'
+    return date_for_format.strftime(format_pattern)
+
+
 class ErgParser:
+    MAX_DEPTH = 30
+    SHIFT = 1
+    DEFAULT = 1
+
     def __init__(self):
-        self.today = datetime.today()
+        pass
+
+    def run(self, mode, depth=1):
+        if mode == ErgRunMode.PARTIAL_UPDATE:
+            self.partial_download(depth)
+        elif mode == ErgRunMode.FULL_UPDATE:
+            self.full_download()
+        elif mode == ErgRunMode.PROCESS:
+            self.process_data()
+            return
+        else:
+            return
+        self.process_data()
 
     def full_download(self):
-        pass
+        for method in ErgMethod:
+            self.download_chunks(depth=self.MAX_DEPTH, method=method.value)
 
-    def partial_download(self):
-        pass
+    def partial_download(self, depth):
+        for method in ErgMethod:
+            self.download_chunks(depth=depth, method=method.value)
 
-    def download_addresses(self):
-        pass
+    @staticmethod
+    def generate_dates(depth):
+
+        today = datetime.today()
+        current_year = today.year
+        prev = format_date(date(year=current_year - depth - ErgParser.SHIFT,
+                                month=ErgParser.DEFAULT, day=ErgParser.DEFAULT))
+        for i in range(current_year - depth, today.year + ErgParser.SHIFT):
+            d = format_date(date(year=i, month=ErgParser.DEFAULT, day=ErgParser.DEFAULT))
+            yield prev, d
+            prev = d
+        yield d, format_date(date(year=current_year + 1, month=ErgParser.DEFAULT, day=ErgParser.DEFAULT))
+
+    def download_chunks(self, depth, method):
+        for start_date, end_date in self.generate_dates(depth):
+            data_chunk = download_egr_data(method, start_date, end_date)
+            path = join(os.path.dirname(__file__), "data", method, f'{method}_{str(start_date)}_{str(end_date)}.csv')
+            data_chunk.to_csv(path, index=False)
+
+    @staticmethod
+    def process_data():
+        processor = EgrDataProcess()
+        processor.process_adresses()
+        processor.process_events()
+        processor.process_ved()
+        processor.process_base_info()
+        processor.process_short_info()
+
+
+if __name__ == "__main__":
+    parser = ErgParser()
+    parser.run(ErgRunMode.PROCESS)
