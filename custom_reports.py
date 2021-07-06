14d1a2171c4e3d7cdfcd2ac1ea122e88cb3d8d30
crax <cronergy100@mail.ru>
2021-07-07 02:10:51 +0300

    add custom_reports.py

---
+++ custom_reports.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from egr_data_process import EgrDataProcess
+from posixpath import join
+
+
+class CustomReports:
+    CUSTOM_REPORTS_DIR = "custom_reports"
+
+    def __init__(self):
+        self.processor = EgrDataProcess()
+
+    def update_reports(self):
+        self.get_new_clients()
+
+    def get_new_clients(self, gap=6):
+        interval_start = (pd.Timestamp.utcnow() - pd.Timedelta(days=gap)).ceil('D')
+
+        base_info = pd.read_csv(self.processor.BASE_MODEL)
+        adresses = pd.read_csv(self.processor.ADRESSES_MODEL)
+        short_info = pd.read_csv(self.processor.SHORT_INFO_MODEL)
+
+        base_info.dfrom = pd.to_datetime(base_info.dfrom)
+        adresses.dfrom = pd.to_datetime(adresses.dfrom)
+
+        new_active_orgs = base_info[base_info.dfrom >= interval_start]
+        new_adresses = adresses[adresses.dfrom >= interval_start]
+        print(new_adresses.dfrom.max())
+        out = new_adresses.merge(new_active_orgs, on='ngrn').merge(short_info, on='ngrn')
+        out.dfrom_x = out.dfrom_x.dt.date
+        out.dfrom_y = out.dfrom_y.dt.date
+        out.to_excel(join(self.CUSTOM_REPORTS_DIR, "new_clients.xlsx"), index=False)
+        print('max_date', out.dfrom_y.max())
